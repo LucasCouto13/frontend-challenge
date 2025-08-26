3facc13361fabca39b7a93d759bfdb61dcdf17d1
LucasCouto13 <lucas.couto@expertcode.com.br>
2025-08-26 14:00:20 -0300

    feat: first commit

---
+++ capputeno-challenge-rockseat/src/app/layout.tsx
@@ -0,0 +1,14 @@
+import { Geist, Geist_Mono, Saira } from "next/font/google";
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
